7fe0222848d3052a6128c30eeed08ead190704c5
Artem <a.kalugin@dunice.net>
2023-10-03 17:04:39 +0300

    add r3f-native-orbitcontrols

--- App.js
+++ App.js
@@ -1,68 +1,23 @@
-import React, {Suspense, useRef} from "react";
-import {StyleSheet} from "react-native";
+import React, {Suspense} from "react";
+import {StyleSheet, View} from "react-native";
 import {Canvas} from "@react-three/fiber";
+import useControls from "r3f-native-orbitcontrols"
 import "react-native-gesture-handler";
-import {GestureDetector, GestureHandlerRootView, Gesture} from "react-native-gesture-handler";
-import Animated from "react-native-reanimated";
 
 import Model from "./components/Model";
 
-const MIN_SCALE = 1.5;
-const DEFAULT_SCALE = 4;
-const MAX_SCALE = 10;
-
 export default function App() {
-    const rotationX = useRef(-25);
-    const rotationY = useRef(0);
-    const scale = useRef(DEFAULT_SCALE);
-
-    const gesture = Gesture.Pan()
-        .runOnJS(true)
-        .onChange((e) => {
-            rotationX.current += e.changeX;
-            rotationY.current += e.changeY;
-        })
-
-    const zoomGesture = Gesture.Pinch()
-        .runOnJS(true)
-        .onUpdate((event) => {
-            const currentZoom = scale.current * event.scale;
-            const inRange = currentZoom > MIN_SCALE && currentZoom < MAX_SCALE;
-
-            if (inRange) {
-                scale.current = scale.current * event.scale;
-            }
-        })
-
-    const composed = Gesture.Simultaneous(gesture, zoomGesture);
+    const [OrbitControls, events] = useControls()
 
     return (
-        <GestureHandlerRootView style={styles.container}>
-            <GestureDetector gesture={composed}>
-                <Animated.View style={{flex: 1}}>
-                    <Canvas
-                        pointerEvents="none"
-                        camera={{fov: 75, near: 0.1, far: 1000, position: [0, (180 * Math.PI) / 180, 10]}}
-                    >
-                        <ambientLight intensity={1} />
-                        <Suspense fallback={null}>
-                            <Model rotationX={rotationX} rotationY={rotationY} scale={scale}></Model>
-                        </Suspense>
-                    </Canvas>
-                </Animated.View>
-            </GestureDetector>
-        </GestureHandlerRootView>
+        <View style={{flex: 1}} {...events}>
+            <Canvas>
+                <OrbitControls enablePan={false} minZoom={2} maxZoom={10}/>
+                <ambientLight intensity={1} />
+                <Suspense fallback={null}>
+                    <Model></Model>
+                </Suspense>
+            </Canvas>
+        </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-    },
-    button: {
-        width: 50,
-        height: 50,
-        backgroundColor: "pink",
-    }
-});
--- components/Model.js
+++ components/Model.js
@@ -1,65 +1,26 @@
+import React from "react";
 import {useGLTF, useTexture} from "@react-three/drei/native";
-import React, {useRef} from "react";
-import {useFrame} from "@react-three/fiber";
 
-const modelPath = require('../assets/models/model.glb');
+const modelPath = require("../assets/models/model.glb");
+const textureTopPath = require("../assets/textures/textures1/top.png");
+const textureBottomPath = require("../assets/textures/textures1/bottom.png");
 
-const textureTopPath = require('../assets/textures/textures1/top.png');
-const textureBottomPath = require('../assets/textures/textures1/bottom.png');
+const DEFAULT_SCALE = 2;
+const DEFAULT_ROTATE_Y = -25 * Math.PI / 180;
 
-const MAX_X_ROTATION = 90;
-const MIN_X_ROTATION = -90;
-const DEFAULT_SCALE = 4;
-
-const Model =({rotationX, rotationY, scale}) => {
+const Model =() => {
     const obj = useGLTF(modelPath);
     const [textureTop, textureBottom] = useTexture([textureTopPath, textureBottomPath]);
-    const meshHead = useRef(null);
-    const meshTop = useRef(null);
-    const meshBottom = useRef(null);
-
-    useFrame(() => {
-
-        if (meshTop.current) {
-
-            const inRange = rotationY.current > MIN_X_ROTATION && rotationY.current < MAX_X_ROTATION;
-
-            if (inRange) {
-                meshHead.current.rotation.x =
-                    meshBottom.current.rotation.x =
-                        meshTop.current.rotation.x =
-                            (rotationY.current * Math.PI) / 180;
-            }
-            meshHead.current.rotation.y =
-                meshBottom.current.rotation.y =
-                    meshTop.current.rotation.y =
-                        ((rotationX.current) * Math.PI) / 180;
-
-            meshHead.current.scale.x =
-                meshBottom.current.scale.x =
-                    meshTop.current.scale.x =
-                        scale.current;
-            meshHead.current.scale.y =
-                meshBottom.current.scale.y =
-                    meshTop.current.scale.y =
-                        scale.current;
-
-            meshHead.current.scale.z =
-                meshBottom.current.scale.z =
-                    meshTop.current.scale.z =
-                        scale.current;
-        }
-    })
 
     return (
         <group>
-            <mesh geometry={obj.nodes.N00b3_1.geometry} ref={meshHead} rotation={[0.5, 0, 0]} scale={DEFAULT_SCALE}>
+            <mesh geometry={obj.nodes.N00b3_1.geometry} rotation={[0, DEFAULT_ROTATE_Y, 0]} scale={DEFAULT_SCALE}>
                 <meshBasicMaterial map={obj.nodes.N00b3_1.material.map} map-flipY={false} />
             </mesh>
-            <mesh geometry={obj.nodes.roblox_model_top.geometry} ref={meshTop} rotation={[0.5, 0, 0]} scale={DEFAULT_SCALE}>
+            <mesh geometry={obj.nodes.roblox_model_top.geometry} rotation={[0, DEFAULT_ROTATE_Y, 0]} scale={DEFAULT_SCALE}>
                 <meshBasicMaterial map={textureTop} map-flipY={false} />
             </mesh>
-            <mesh geometry={obj.nodes.roblox_model_bot.geometry} rotation={[0.5, 0, 0]} ref={meshBottom} scale={DEFAULT_SCALE}>
+            <mesh geometry={obj.nodes.roblox_model_bot.geometry} rotation={[0, DEFAULT_ROTATE_Y, 0]} scale={DEFAULT_SCALE}>
                 <meshBasicMaterial map={textureBottom} map-flipY={false} />
             </mesh>
         </group>
